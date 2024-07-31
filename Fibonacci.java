e2fd940232ee45979846904dd3e1b18bd6a9d402
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 13:33:02 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+package Integer;
+import java.util.Scanner;
+public class Fibonacci {
+public void scan() {
+	int a,b,c,d;	
+	Scanner in=new Scanner(System.in);
+	System.out.println("Give Your Number");
+	a=in.nextInt();
+	System.out.println("Give Sequential Number");
+	b=in.nextInt();
+	System.out.println("Give Your Count");
+	d=in.nextInt();
+	System.out.println("First Number"+a);
+	System.out.println("Second Number"+b);
+// prepare Logic to find out Fibanocci series and print it
+}	
+	public static void main(String[] args) {
+		Fibonacci f=new Fibonacci();
+f.scan();		
+
+
+}
+}
