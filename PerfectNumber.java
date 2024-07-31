58d2cbf5919727ad43107e8220dc054c014f611f
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 13:37:26 +0530

    Create PerfectNumber.java

---
+++ PerfectNumber.java
@@ -0,0 +1,15 @@
+public class PerfectNunmber{
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter your Number");
+	int temp=0;
+	int a=in.nextInt();
+// Write Logic to find out whether a Number is  Perfect Number or not
+		
+	}
+	if(temp==a) {
+		System.out.println("Perfect Number"+a);
+	}
+
+}
+}
