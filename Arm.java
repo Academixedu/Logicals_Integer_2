23f07727e4969187a299189398930a1c69834723
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-08-01 00:30:58 +0530

    Update Arm.java

--- Arm.java
+++ Arm.java
@@ -27,19 +27,14 @@ public class Arm {
         
       // Write the Logic to know the sum value
       while (a > 0) {
-        b = a % 10;
-//            
-        int power = 1;
-//          
-        for (int i = 1; i <=n; i++) {
-//            	
+        b = a % 10;            
+        int power = 1;       
+        for (int i = 1; i <=n; i++) {           	
             power *= b;
 //                power*=1*(3*3*3) three iterations if we take 153
         }
-        sum += power;
-//            
-        a = a / 10;
-//          
+        sum += power;           
+        a = a / 10;         
     }
     
         if (temp == sum) {
