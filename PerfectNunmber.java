3d2bdec83a3664d39f32cb00bf8888c06275c3ed
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-08-01 00:28:28 +0530

    sarvesh

---
+++ PerfectNunmber.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class PerfectNunmber{
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter your Number");
+	int temp=0;
+	int a=in.nextInt();
+
+	for(int i=1;i<a;i++){
+	if(a%i==0){
+		temp=temp+i;
+		System.out.println(i);
+		
+	}
+}
+// Write Logic to find out whether a Number is  Perfect Number or not
+if(temp==a) {
+	System.out.println("Perfect Number"+a);
+}
+}}
+	
+
+
+
