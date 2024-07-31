d8e276ceb92c74c8498689c69d564494c8251ebd
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 13:35:52 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,10 @@
+public class Palindrome{
+  public static void main(String[]args){
+	Scanner in=new Scanner(System.in);
+System.out.println("Enter");
+	int	a=in.nextInt();
+	int temp=0;
+	int pal=a;
+    // Write your Logic to find out a number ArmStrong or not
+  }
+}
