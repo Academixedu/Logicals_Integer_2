6e36993caf43b62d8d5d138b259bd16bf484bb62
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-07-31 16:01:49 +0530

    Assignment

--- Arm.java
+++ Arm.java
@@ -24,9 +24,16 @@ public class Arm {
         int a = in.nextInt();
         int temp = a;
         int n = countDigits(a); // Calculate the number of digits in the input number
-        
-      // Write the Logic to know the sum value
-    
+        // Write the Logic to know the sum value
+        while (a > 0) {
+            b = a % 10;
+            int power = 1;
+            for (int i = 1; i <=n; i++) {
+                power *= b;
+            }
+            sum += power;
+            a = a / 10;
+        }
         if (temp == sum) {
             System.out.println(temp + " is an Armstrong");
         } else {
--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Palindrome{
   public static void main(String[]args){
 	Scanner in=new Scanner(System.in);
@@ -5,6 +7,18 @@ System.out.println("Enter");
 	int	a=in.nextInt();
 	int temp=0;
 	int pal=a;
+	while(a!=0){
+            int rem=a%10;
+            temp=temp*10+rem;
+            a=a/10;
     // Write your Logic to find out a number ArmStrong or not
   }
+  if(pal==temp){
+	System.out.println("Palindrome number");
+	
+}
+else{
+	System.out.println("NOt a Palindrome number");
+}
+}
 }
