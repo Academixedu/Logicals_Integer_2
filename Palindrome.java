70b85d3de0994869017ec9dd2f5af841ac8d1091
goutham120203 <gouthampaladugu12@gmail.com>
2024-08-02 21:50:01 +0530

    goutham

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Palindrome{
   public static void main(String[]args){
 	Scanner in=new Scanner(System.in);
@@ -6,5 +7,16 @@ System.out.println("Enter");
 	int temp=0;
 	int pal=a;
     // Write your Logic to find out a number ArmStrong or not
+	while(a!=0){
+		int rem = a%10;
+		temp = temp*10+rem;
+		a = a/10;
+	}
+	if(pal == temp){
+		System.out.println("panlidrome");
+	}
+	else{
+		System.out.println("not panlidrome");
+	}
   }
 }
