38593381fbcac280373ce3d052255bb458818e66
Bharathmanne18 <bharathmanne12@gmail.com>
2024-08-02 17:36:07 +0530

    Update PerfectNumber.java

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,15 +1,26 @@
-public class PerfectNunmber{
-public static void main(String[] args) {
-	Scanner in=new Scanner(System.in);
-	System.out.println("Enter your Number");
-	int temp=0;
-	int a=in.nextInt();
-// Write Logic to find out whether a Number is  Perfect Number or not
-		
-	}
-	if(temp==a) {
-		System.out.println("Perfect Number"+a);
-	}
+ 
+import java.util.Scanner;
 
+ 
+public class PerfectNunmber{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter your Number");
+		int temp=0;
+		int a=in.nextInt();
+	// Write Logic to find out whether a Number is  Perfect Number or not
+	for(int i=1;i<a;i++){
+		if(a%i ==0){
+			temp=temp+i;
+			System.out.println(i);
+		}
+	}
+		if(temp==a) {
+			System.out.println("Perfect Number"+a);
+		}
+	else{
+		System.out.println("not perfect number");
+	}
+	}
 }
-}
+	
